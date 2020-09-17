33c300af09269c44a1f83cf5363e1057bae71808
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-17 17:30:30 -0300

    superclasse e classes funcionando

--- CadastroCF.py
+++ CadastroCF.py
@@ -9,16 +9,26 @@ class Pessoa:#super classe
         self.__cpf = cpf
         self.__rg = rg
         self.__sexo = sexo
-        
+
     def imprimir_dados(self):
         """Imprimi as informações pessoais"""
         return f'{self.__nome} {self.__sobrenome}'
-    
+
 class Cliente(Pessoa):
     def __init__(self, nome, sobrenome, cpf, rg, sexo, saldo):
+        """Retorna as variaveis da superclasse + saldo"""
         super().__init__(nome, sobrenome, cpf, rg, sexo)##herança da super classe
         self.__saldo = saldo
-        
-        
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, sobrenome, cpf, rg, sexo, matricula):
+        """Retorna as variaveis da superclasse + matricula"""
+        super().__init__(nome, sobrenome, cpf, rg, sexo)
+        self.__matricula = matricula
+
+
 cliente1 = Cliente('marcelo', "claro", 22233366656, 986574959, "M", 5000)
 print(cliente1.imprimir_dados())
+
+funcionario1 = Funcionario("Gertrudez", "Python", 74185298754, 784596824, "F", "BB1569AP96")
+print(funcionario1.imprimir_dados())
